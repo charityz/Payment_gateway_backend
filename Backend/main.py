2a9184f3d553efb7fb092ef6555de1d6b60c8e99
charityz <ezecharity194@gmail.com>
2025-09-19 13:25:27 +0100

    fix cors again again

--- Backend/main.py
+++ Backend/main.py
@@ -35,11 +35,11 @@ if os.path.isdir(WELL_KNOWN_DIR):
 
 origins = [
     # "https://payment-gateway-3.onrender.com",
-    "http://127.0.0.1:5000",
-    'https://payverge.netlify.app',
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "http://127.0.0.1:5000",
     "http://localhost:3000",
+    'https://payverge.netlify.app',
 ]
 
 app.add_middleware(
